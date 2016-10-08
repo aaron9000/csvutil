c5e1bda9639166edc4e65b0ee5934d24375810a6
Aaron Geisler <aaron1@Aarons-MacBook-Pro.local>
2016-10-07 19:21:01 -0700

    handling commas elegantly in preview

--- test/edge_case_test.js
+++ test/edge_case_test.js
@@ -16,6 +16,8 @@ function textFromFile(relativePath:String):String{
 describe("edge cases", () => {
 
     const messy_md = textFromFile('./data/messy_md.txt');
+    const messy_csv = textFromFile('./data/messy_csv.txt');
+    const messy_pretty = textFromFile('./data/messy_pretty.txt');
 
     describe.only("diff", () => {
 
@@ -32,6 +34,39 @@ describe("edge cases", () => {
                     done();
                 })
                 .catch(() => done('fail'));
+        });
+
+        it("compares renders messy file with escaped string (pretty)", (done) => {
+            handleConfig({
+                pathA: 'test/data/messy.csv',
+                rows: 0,
+                output: 'pretty',
+                width: 16,
+                buildStringBuffer: true
+            })
+                .then(v => {
+                    expect(v.stringBuffer).to.be.equal(messy_pretty);
+                    done();
+                })
+                .catch(() => done('fail'));
+        });
+
+        it("compares renders messy file with escaped string (csv)", (done) => {
+            handleConfig({
+                pathA: 'test/data/messy.csv',
+                rows: 0,
+                output: 'csv',
+                width: 16,
+                buildStringBuffer: true
+            })
+                .then(v => {
+                    expect(v.stringBuffer).to.be.equal(messy_csv);
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done('fail');
+                });
         });
 
         //it("returns error for two nonexistent files", (done) => {
